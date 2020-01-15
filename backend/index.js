2f3c93fafa90c6046677fed8276b57f00b5157f0
savio <saviopf22@gmail.com>
2020-01-14 21:39:56 -0300

    logs das requisições ao servidor

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,10 @@ const port = 7777
 //app.use()
 
 app.get('/', (req, res) => {
+    console.log(req.method, req.path)
     return res.send('hello world')
 })
 
-app.listen(port, (response) => {
-    console.log(`logs do localhost:${port}~> ${response}`)
+app.listen(port, () => {
+    console.log(`logs do localhost:${port}`)
 })
