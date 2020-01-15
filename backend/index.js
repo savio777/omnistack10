89c47b9cac9ae243dceb7867366c879e69ec7a2e
savio <saviopf22@gmail.com>
2020-01-14 22:35:04 -0300

    conexão com mongodb e primeiros testes da api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/omnistack10', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 const port = 7777
 
