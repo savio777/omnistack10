52e66a98e925b927f7bec0f4856dd9422cf8fc2f
savio <saviopf22@gmail.com>
2020-01-14 22:16:12 -0300

    brincando com o http

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,48 @@ const app = express()
 
 const port = 7777
 
-//app.use()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     console.log(req.method, req.path)
-    return res.json({ message: 'hello world' })
+    return res.json({
+        message: 'hello world',
+        query: req.query.name,
+        body: req.body.test,
+        header: req.headers.token_example
+    })
 })
+
+app.post('/', (req, res) => {
+    console.log(req.method, req.path)
+    return res.json({
+        message: 'hello world',
+        query: req.query.name,
+        body: req.body.test,
+        header: req.headers.token_example
+    })
+})
+
+app.put('/', (req, res) => {
+    console.log(req.method, req.path)
+    return res.json({
+        message: 'hello world',
+        query: req.query.name,
+        body: req.body.test,
+        header: req.headers.token_example
+    })
+})
+
+app.delete('/', (req, res) => {
+    console.log(req.method, req.path)
+    return res.json({
+        message: 'hello world',
+        query: req.query.name,
+        body: req.body.test,
+        header: req.headers.token_example
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`logs do localhost:${port}`)
