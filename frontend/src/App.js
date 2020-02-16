39426a937ca1852f51e51b46c42b8808d93043e5
savio <saviopf22@gmail.com>
2020-02-16 19:26:09 -0300

    frontend quase todo completo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,108 @@
-import React, { useState } from 'react'
-import Header from './HeaderTest'
-
-// Componentes ~> Bloco isolado de HTML, CSS e JS
-// Propriedades ~> Informações que um componente Pai passa para o componente Filho
-// Estados ~> Informações mantidas pelo componente de forma imutável
+import React, { useState, useEffect } from 'react'
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 const App = () => {
 
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (postition) => {
+        setLongitude(postition.coords.longitude)
+        setLatitude(postition.coords.latitude)
+      },
+      (error) => {
+        console.log('error geolocation~> ', error)
+      },
+      {
+        timeout: 30000
+      }
+    )
+  }, [])
+
+  const [githubUser, setGithubUser] = useState('')
+  const [techs, setTechs] = useState('')
+  const [latitude, setLatitude] = useState('')
+  const [longitude, setLongitude] = useState('')
+  const [repeticaoTeste, setRepeticaoTeste] = useState([1, 2, 3, 4, 5])
 
   return (
-    <>
-      <Header text='Olá' value={count} />
-      <Header text='Hello' value={count} />
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_user">Usuário do Github</label>
+            <input
+              name='github_user'
+              id='github_user'
+              value={githubUser}
+              onChange={(e) => setGithubUser(e.target.value)}
+              required
+            />
+          </div>
 
-      <button onClick={() => setCount(count + 1)}>Clique</button>
-    </>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name='techs'
+              id='techs'
+              value={techs}
+              onChange={(e) => setTechs(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                name='latitude'
+                id='latitude'
+                value={latitude}
+                onChange={(e) => setLatitude(e.target.value)}
+                required
+                disabled
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                name='longitude'
+                id='longitude'
+                value={longitude}
+                onChange={(e) => setLongitude(e.target.value)}
+                required
+                disabled
+              />
+            </div>
+          </div>
+          <button type='submit'>Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          {(repeticaoTeste) && (repeticaoTeste.map(value => (
+
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/35678887?s=460&v=4" alt="Sávio" />
+                <div className="user-info">
+                  <strong>Sávio</strong>
+                  <span>React, Nodejs</span>
+                </div>
+              </header>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+              <a href="https://github.com/savio777">Acessar perfil</a>
+            </li>
+          )))}
+
+        </ul>
+      </main>
+    </div>
   )
 }
 
--- frontend/src/HeaderTest.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = (props) => {
-  return (
-    <h1>{props.text} | {props.value}</h1>
-  )
-}
-
-export default Header
