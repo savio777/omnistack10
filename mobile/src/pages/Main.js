80c9dec3ad3560a4466670f7b27fbbf43cfaac9c
savio777 <saviopf22@gmail.com>
2020-03-25 02:51:41 -0300

    instalando maps e geolocation

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+
+import Geolocation from '@react-native-community/geolocation';
+import MapView from 'react-native-maps';
 
 function Main(props) {
+
+  const [lat, setLat] = useState('')
+  const [long, setLong] = useState('')
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log('pos~> ', position)
+      },
+      error => Alert.alert('Error', JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }, [])
+
   return (
     <View>
       <TouchableOpacity onPress={() => props.navigation.navigate('Profile')}>
-        <Text>oi</Text>
+        <Text style={{ textAlign: 'center' }}>Perfil</Text>
+        <MapView style={styles.map} />
       </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flexGrow: 1,
+    height: '100%',
+    width: '100%'
+  }
+})
 
 export default Main
